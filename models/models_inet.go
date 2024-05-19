3896c9a51c5e2abca280a411559ab6777e75f9dd
Kitsaponjim <98834570+Kitsaponjim@users.noreply.github.com>
2024-05-19 20:25:32 +0700

    update

--- models/models_inet.go
+++ models/models_inet.go
@@ -2,10 +2,10 @@ package models
 
 type Person struct {
 	Email       string `json:"email" validate:"required,email"`
-	Username    string `json:"username" validate:"required,min=5,max=20"`
-	Password    string `json:"password" validate:"required"`
+	Username    string `json:"username" validate:"required,alphanum"`
+	Password    string `json:"password" validate:"required,min=6,max=20"`
 	IdLine      string `json:"idLine" validate:"required"`
 	Phone       string `json:"phone" validate:"required"`
 	Type        string `json:"type" validate:"required"`
-	NameWebsite string `json:"nameWebsite" validate:"required"`
+	NameWebsite string `json:"nameWebsite" validate:"required,min=2,max=30,alphanum"`
 }
